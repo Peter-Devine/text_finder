4c66a7d03b9301d1b4d9544e02a6913a08f4122e
Peter Devine <pdev438@UoA.auckland.ac.nz>
2021-02-02 19:12:17 +1300

    Add SVD to make larger text corpora browsable

--- embedding.py
+++ embedding.py
@@ -1,16 +1,28 @@
 from sentence_transformers import SentenceTransformer
 import tensorflow_hub as hub
 import numpy as np
+from sklearn.decomposition import TruncatedSVD
+
+MAX_COMPONENTS = 50
 
 def normalize(enc):
     # We normalize embeddings so that the average magnitude of vectors is 1
     return  enc / (np.linalg.norm(enc, axis=1).mean())
 
 def encode_text(text_list):
+    # Load models
     sbert_model = SentenceTransformer("bert-large-nli-mean-tokens")
     use_model = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")
 
+    # Get embeddings
     sbert_embedding = sbert_model.encode(text_list, show_progress_bar=True)
     use_embedding = use_model(text_list).numpy()
 
-    return np.concatenate([use_embedding, sbert_embedding], axis=1)
+    # Concatenate embeddings together
+    concat_embedding = np.concatenate([use_embedding, sbert_embedding], axis=1)
+
+    # Initialise SVD to reduce dimensionality
+    n_components = min([MAX_COMPONENTS, conat_embedding.shape[0], conat_embedding.shape[1]])
+    svd = TruncatedSVD(n_components=n_components)
+
+    return svd.fit_transform(concat_embedding)
